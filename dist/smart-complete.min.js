ace609c4510111a1df601275322ddb55ee15ec49
zskymn <zskymn@163.com>
2016-02-25 15:44:14 +0800

    更新readme

--- dist/smart-complete.min.js
+++ dist/smart-complete.min.js
@@ -1,5 +1,5 @@
 /*!
- * smart-complete - an angularjs directive which can complete expressions for input or textarea
+ * smart-complete - 为input和textarea提供提示补全功能的AngularJS指令
  * @version 2.0.0
  * @link https://github.com/zskymn/smart-complete#readme
  */
